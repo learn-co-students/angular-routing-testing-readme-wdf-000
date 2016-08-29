7076b3a2a91c187acd2da9a3cea94bee9170b97a
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-29 11:31:14 -0400

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -1,19 +1,59 @@
 describe('Routes', function () {
-	var $state;
+  var $state;
 
-	beforeEach(module('app'));
+  beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$state = $injector.get('$state');
-	}));
+  beforeEach(inject(function ($injector) {
+    $state = $injector.get('$state');
+  }));
 
-	describe('Settings', function () {
-		var state;
+  describe('Settings', function () {
+    var state;
 
-		it('should have the correct URL', function () {
-			state = $state.get('settings');
+    it('should have the correct URL', function () {
+      state = $state.get('settings');
 
-			expect(state.url).toEqual('/settings');
-		});
-	});
+      expect(state.url).toEqual('/settings');
+    });
+
+    it('should use the correct template', function () {
+      expect(state.templateUrl).toEqual('views/settings.html');
+    });
+
+    it('should use the right controller', function () {
+      expect(state.controller).toEqual('SettingsController');
+    })
+  });
+
+  describe('UserSettings', function () {
+    var state;
+    it('should have the correct URL', function () {
+      state = $state.get('settings.user');
+      expect(state.url).toEqual('/user');
+    });
+
+    it('should use the correct template', function () {
+      expect(state.templateUrl).toEqual('views/settings/user.html');
+    });
+
+    it('should use the right controller', function () {
+      expect(state.controller).toEqual('UserSettingsController');
+    })
+  });
+
+  describe('NotificationsSettings', function () {
+    var state;
+    it('should have the correct URL', function () {
+      state = $state.get('settings.notifications')
+	expect(state.url).toEqual('/notifications');
+    });
+
+    it('should use the correct template', function () {
+      expect(state.templateUrl).toEqual('views/settings/notifications.html');
+    });
+
+    it('should use the right controller', function () {
+      expect(state.controller).toEqual('NotificationsSettingsController');
+    })
+  });
 });
